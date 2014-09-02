19ad879cb880342da1de5325b56c1559065e7762
Dion van 't Veer <dionnv@gmail.com>
2014-09-02 08:22:29 +0200

    Added TradeTrackerReader
    TradeTrackerReader will read productfeed.xml from a given location and
    will only put the data in an object file called "Product".

---
+++ TradeTrackerReader/TradeTrackerReader/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeTrackerReader
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string SKU { get; set; }
+        public string DeliveryCost { get; set; }
+        public string DeliveryTime { get; set; }
+        public string EAN { get; set; }
+        public string Stock { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+
+    }
+}
