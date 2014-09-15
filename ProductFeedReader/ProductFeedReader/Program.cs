ca27a768718986bf8805aa41127f25be3cbd6568
Dion van 't Veer <dionnv@gmail.com>
2014-09-15 15:06:47 +0200

    Added Statics, Util, Program and AffiliateBase comments.

--- ProductFeedReader/ProductFeedReader/Program.cs
+++ ProductFeedReader/ProductFeedReader/Program.cs
@@ -11,7 +11,14 @@ namespace ProductFeedReader
 {
     class Program
     {
+        /// <summary>
+        /// This thread is used by the productfeedreader, being the producer in this application.
+        /// </summary>
         public static Thread producer;
+
+        /// <summary>
+        /// This thread is used by BOB, being the consumer in the application.
+        /// </summary>
         public static Thread consumer;
 
         /// <summary>
@@ -41,13 +48,21 @@ namespace ProductFeedReader
         {
             //Create BOB and start dequeueing items.
             BOB bob = new BOB();
+
+            //Remain alive while thread one isnt done
             while (true)
             {
+                //Take product from queue
                 Product p = ProductQueue.Dequeue(); 
+
+                //Break out of while loop when the queue gives null. This only happens when 
+                //productfeedreader is flagged as done.
                 if(p == null)
                 {
                     break;
                 }
+
+                //Process the product otherwise.
                 bob.Process(p);
             }
             Console.WriteLine("Thread 2 is done.");
@@ -55,8 +70,10 @@ namespace ProductFeedReader
 
         static void ProductFeedReader()
         {
-            //Exectue the ProductFeedReader.
+            //Create productfeedreader object.
             ProductFeedReader pfr = new ProductFeedReader();
+
+            //Start it
             pfr.Start();
             Console.WriteLine("Thread 1 is done.");
         }
@@ -73,6 +90,7 @@ namespace ProductFeedReader
 
         static void FinalizeAndClose()
         {
+            //Close everything and shut down.
             Console.WriteLine("Writing data to logfile...");
             Statics.Logger.Close();
             Console.WriteLine("Done.");
