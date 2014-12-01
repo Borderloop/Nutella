b5a2b7e9728a074b20d85afe5af011f3b22be66d
Marnik <marnikverschoor@gmail.com>
2014-12-01 17:27:58 +0100

    Added bol crawler

---
+++ Crawler/src/Boris.py
@@ -0,0 +1,59 @@
+import os
+import glob
+from ConfigParser import SafeConfigParser
+from threading import Thread
+
+from Crawlers import TradeTracker
+from Crawlers import FeedCrawler
+#from Crawlers import TradeDoubler
+from Crawlers import Belboon
+from Crawlers import Daisycon
+from Crawlers import Bol
+
+global productFeedsPath
+
+
+def main():
+    parseConfigFile()
+    emptyDirectories()
+    startCrawlers()
+
+
+# Procedure to parse the config file
+def parseConfigFile():
+    parser = SafeConfigParser()
+    parser.read('C:/BorderSoftware/Boris/settings/boris.ini')
+    global productFeedsPath
+    productFeedsPath = parser.get('General', 'productfeedspath')
+
+
+# This procedure empties all the product feed folders.
+def emptyDirectories():
+
+    productFeedPaths = [x[0] for x in os.walk(productFeedsPath)]
+
+    for path in productFeedPaths:
+        if path != productFeedPaths[0] and 'BorderBot' not in path:
+            files = glob.glob(path + '/*')
+            for f in files:
+                os.remove(f)
+
+
+# This procedure starts all crawlers.
+def startCrawlers():
+    t = Thread(target=TradeTracker.Crawler().main)
+    t.start()
+
+    t = Thread(target=FeedCrawler.Crawler().main)
+    t.start()
+
+    t = Thread(target=Belboon.Crawler().main)
+    t.start()
+
+    t = Thread(target=Daisycon.Crawler().main)
+    t.start()
+
+    t = Thread(target=Bol.Crawler().main)
+    t.start()
+
+main()
